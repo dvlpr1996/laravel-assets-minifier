3c50d53097a1537ec2932474c219de36ab6b2e3b
dvlpr1996 <nimajahanbakhshian@gmail.com>
2022-05-28 17:57:02 +0430

    implement fileController with Services for basic actions

--- app/Http/Controllers/fileController.php
+++ app/Http/Controllers/fileController.php
@@ -13,10 +13,15 @@ class fileController extends Controller
 
 	public function __construct(StorageManager $StorageManager)
 	{
-		$this->filename = time() . mt_rand() . "-Laravel-minifier.zip";
+		$this->filename =  mt_rand(1, time()) . "-Laravel-minifier.zip";
 		$this->pathToDownload = storage_path("app/public/download/" . $this->filename);
 		$this->StorageManager = $StorageManager;
 	}
+
+	// public function __construct(
+	// 	private StorageManager $StorageManager,
+	// 	private FileHandler $FileHandler
+	// ) {}
 
 	public function index()
 	{
@@ -25,23 +30,16 @@ class fileController extends Controller
 
 	public function upload(FileUploadRequest $request)
 	{
-		if ($request->hasFile("files")) {
-			foreach ($request->file("files") as $files) {
-				$files->store("upload");
-			}
-		}
-		return back()->with("success", "your files successfully uploaded");
+		return $this->StorageManager->upload($request);
 	}
 
 	public function download()
-	{	
-		// delete after downloaded
+	{
 		return $this->StorageManager->download($this->pathToDownload);
 	}
 
 	public function delete()
 	{
-		$this->StorageManager->delete("upload");
-		return back()->with("success", "your files successfully deleted");
+		return $this->StorageManager->delete("upload");
 	}
 }
